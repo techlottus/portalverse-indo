3015e191abce8cd275c50a1985c6404a88d0e17e
Maria-cris <maria.delacruz@lottuseducation.com>
2025-05-09 15:10:08 -0600

    remove cont ifremsrc

--- public/whatsapp-widget.js
+++ public/whatsapp-widget.js
@@ -153,7 +153,7 @@
     // const currentUrl = encodeURIComponent(window.location.href);
     // const userAgent = encodeURIComponent(navigator.userAgent);
     // const iframeSrc = `${base_url}/whatsapp-form-page/?phone=${whatsapp_phone}&${window.location.search.substring(1)}&source=Widget WhatsApp ${source}&event_source_url=${currentUrl}&client_user_agent=${userAgent}`;
-    const iframeSrc = `${base_url}/whatsapp-form-page/?phone=${whatsapp_phone}&${window.location.search.substring(1)}&source=Widget WhatsApp ${source}`;
+    // const iframeSrc = `${base_url}/whatsapp-form-page/?phone=${whatsapp_phone}&${window.location.search.substring(1)}&source=Widget WhatsApp ${source}`;
     
     chatWidget.innerHTML = `
       <div id="chat-frame-widget" class="shadow-xl">
@@ -170,7 +170,7 @@
             </svg>
           </button>
         </div>
-        <iframe id="iframe" src="${iframeSrc}" style="  width: 300px; min-width:200px; max-width:320px; height: -webkit-fill-available;height:100%; opacity: 1; color-scheme: none; background: white !important; margin: 0px; max-height: -webkit-fill-available; max-height:-moz-available; z-index: 999999999 !important; border-bottom-left-radius: 0.5rem; border-bottom-right-radius: 0.5rem;padding-right: 16px;padding-left: 16px; border:none;"></iframe>
+        <iframe id="iframe" src="${base_url}/whatsapp-form-page/?phone=${whatsapp_phone}&${window.location.search.substring(1)}&source=Widget WhatsApp ${source}" style="  width: 300px; min-width:200px; max-width:320px; height: -webkit-fill-available;height:100%; opacity: 1; color-scheme: none; background: white !important; margin: 0px; max-height: -webkit-fill-available; max-height:-moz-available; z-index: 999999999 !important; border-bottom-left-radius: 0.5rem; border-bottom-right-radius: 0.5rem;padding-right: 16px;padding-left: 16px; border:none;"></iframe>
       </div>
       <button id="btn-trigger-chat">
         <svg class="text-surface-0 h-9 w-9" width="100%" height="100%" viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg">
