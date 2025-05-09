766c203c53cd16c6997e2c8708d4e7ee8c4beaf9
Maria-cris <maria.delacruz@lottuseducation.com>
2025-05-08 18:03:58 -0600

    added capi

--- forms/LeadWhatsappForm.tsx
+++ forms/LeadWhatsappForm.tsx
@@ -7,6 +7,8 @@ import cn from "classnames"
 
 type LeadWhatsappFormTypes = {
   leadPhone?: string | string[];
+  event_name?: string | string[] | null;
+  event_source?: string | string[] | null;
   utmData?: {
     utm_content?: string | string[] | null;
     utm_term?: string | string[] | null;
@@ -44,7 +46,7 @@ export const LeadWhatsappForm = (props: LeadWhatsappFormTypes) => {
    * @property {string} leadPhone - The phone number of the lead.
    * @property {object} utmData - The UTM data associated with the lead, typically used for tracking marketing campaigns.
    */
-  const { leadPhone, utmData, utmLeads } = props;
+  const { leadPhone, utmData, utmLeads, event_name="completeRegistration", event_source="website"  } = props;
 
   /**
    * Retrieves the business unit from the environment variables.
@@ -237,6 +239,12 @@ export const LeadWhatsappForm = (props: LeadWhatsappFormTypes) => {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
+            event_time: Date.now(),
+            send_capi: true,
+            event_source_url: window?.top?.location.origin || window.location.origin || null,
+            client_user_agent: window?.top?.navigator.userAgent || window.navigator.userAgent || null,
+            event_name: event_name,
+            action_source: event_source,
             name: leadData.name,
             last_name: leadData.last_name,
             email: null,
@@ -253,7 +261,8 @@ export const LeadWhatsappForm = (props: LeadWhatsappFormTypes) => {
               "utm_term": utmData?.utm_term || null,
               "gclid": utmData?.gclid || null,
               "source": utmData?.source || null,
-              "utm_leads": utmLeads
+              "utm_leads": utmLeads,
+              "modality": leadData.modality
             },
           }),
         });
